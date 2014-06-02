8d219c3418f897db99d0f3c4efcb575e44e251e5
jstewart983 <nj.jstewart@gmail.com>
2014-06-02 11:27:05 -0500

    New page for week 3 work

---
+++ week3.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<title>Jordan Stewart's Website</title>
+		<link rel="stylesheet" type="text/css" href="main.css"/>
+	</head>	
+<body class = "content">
+	<p class = "menu">
+	<?php include "menu.php"
+	?>
+	</p>
+	<p align = "left">
+	<button onclick="history.go(-1);">Back</button>
+	</p>
+		<h3><strong>Contact Info</strong></h3>
+		<p class="footer">
+		<?php include 'footer.php';?>
+		</p>
+	</body>
+		
+		</html>
