fff33a5d3cc5edf9a4ea5e56ae796411cec032c1
jstewart983 <nj.jstewart@gmail.com>
2014-06-02 11:27:34 -0500

    Added week 3 option

--- menu.php
+++ menu.php
@@ -2,6 +2,7 @@
 <?php echo '<a href="/index.php">Home</a> -
 <a href="/familytree.php">Week 1</a> -
 <a href="/week2.php">Week 2</a> -
+<a href="/week3.php">Week 3</a> -
 <a href="/phpconfig">PHP Configuration</a> -
 <a href="/printphp.php">Print Report</a>';
 ?>
