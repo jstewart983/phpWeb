32c1925c9b7ce1d62cacd9ef83c208b35d25a7db
jstewart983 <nj.jstewart@gmail.com>
2014-06-03 09:39:36 -0500

    Added some spacing/added github repo link

--- index.php
+++ index.php
@@ -6,23 +6,26 @@
 		<title>Jordan Stewart's Website</title>
 		<link rel="stylesheet" type="text/css" href="main.css"/>
 </head>
-
 <body class = "content">
 <p class="menu">
 	<?php include 'menu.php';?>
 	</p>
+	<br></br>
 			<h3><strong>Welcome To My PHP Demonstration Website</strong></h3>
+			<br></br>
 		<p align ="left">
-		
 			For my website, I chose to use Yahoo Small Business Web Hosting (Yahoo). I first looked at Yahoo because Professor Hart suggested it and because it met all the requirements for the class, which included supporting PHP and mySQL5.x. After looking into it further, I found there to be other beneficial aspects. One of the major pros is the price. It is only $10 a month, which is reasonably priced. For smaller, low-traffic sites like the ones we will be creating, it is fairly fast. 
 		</p>
 		<p align="left">
 		If I were using Yahoo for a website that was much larger or more complex than the ones we are creating for this class, I would definitely run into some problems. For starters, they don’t provide access to .htaccess files. It is shared hosting, which means that if you have a very heavy imaged site or have more than 4K-5K visitors in a day, it can run very slowly. It is also been reported from many different review sites that the customer service is very difficult to get a hold of and work with.
 		</p>
 		</p>
-			<p class="footer">
+		<br></br>
+		<p> Here is the link to my repo on Github: <a href = "https://github.com/jstewart983/phpWeb"><strong> jstewart983/phpWeb </strong></a></p>
+		<br></br>
+		<br></br>
+		<p class="footer">
 		<?php include 'footer.php';?>
 		</p>
 	</body>
-	
 		</html>
