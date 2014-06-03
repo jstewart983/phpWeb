35d95d81c1af2468c582f1cb862d6c0f2bed56a5
jstewart983 <nj.jstewart@gmail.com>
2014-06-03 11:25:36 -0500

    Centered header

--- printphp.php
+++ printphp.php
@@ -12,7 +12,7 @@
 <p align = "left">
 	<button onclick="history.go(-1);">Back </button>
 	</p>
-<h3><strong>This is my report printed in PHP</strong></h3>
+<h3 align="center"><strong>This is my report printed in PHP</strong></h3>
 <body>
 <?php print("For my website I chose to use Yahoo Small Business Web Hosting (Yahoo). I first looked at Yahoo because Professor Hart suggested it and because it met all the requirements for the class, which included supporting PHP and mySQL5.x. After looking into it further, I found there to be other beneficial aspects. One of the major pros is the price. It is only $10 a month, which is reasonably priced. For smaller, low-traffic sites like the ones we will be creating, it is fairly fast."); ?> 
 <?php print("If I were using Yahoo for a website that was much larger or more complex than the ones we are creating for this class, I would definitely run into some problems. For starters, they don’t provide access to .htaccess files. It is shared hosting, which means that if you have a very heavy imaged site or have more than 4K-5K visitors in a day, it can run very slowly. It is also been reported from many different review sites that the customer service is very difficult to get a hold of and work with.");?>
