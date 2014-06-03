869d2ee45ba510db6826c17f57daa321ced3ac5a
jstewart983 <nj.jstewart@gmail.com>
2014-06-03 11:19:59 -0500

    Alternate family tree test page

---
+++ familytreetest.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<title>Jordan Stewart's Website</title>
+		<link rel="stylesheet" type="text/css" href="slickmap.css"/>
+		<link rel="stylesheet" type="text/css" href="main.css" />
+
+	<!--[if lte IE 9]> <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script> <![endif]-->
+	
+</head>
+
+<body>
+	<p class = "menu">
+		<?php include 'menu.php';?>
+		</p>
+<p><div class = "content">
+	<div align = "center" class="sitemap">
+		<h1>My Family Tree</h1>
+				<ul id="primaryNav" align = "center">
+					<li><a href="tom.php">Tom</a></li></ul>
+					<ul id = "primaryNav" align = "center">
+					<li><a href="michele.php">Michele</a>
+						<ul id ="primaryNav">
+							<li><a href="jared.php">Jared</a></li>
+							<li><a href="justus.php">Justus</a></li>
+							<li><a href="johnjohn.php">John John</a></li>
+						</ul>
+					</li>
+					
+				</ul>
+
+	</div>
+	</div>
+	</p>
+		<p class="footer">
+		<?php include 'footer.php';?>
+		</p>
+	</body>
+	</html>
