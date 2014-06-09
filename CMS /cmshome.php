a546b5a25f2ea68d2231300028dccc9fa5bd8d7f
Jordan Stewart <nj.jstewart@gmail.com>
2014-06-09 00:40:37 -0400

    Utilized table to layout content

--- CMS /cmshome.php
+++ CMS /cmshome.php
@@ -7,26 +7,36 @@
 		<link rel="stylesheet" type="text/css" href="/main.css"/>
 </head>
 <body class = "content">
-<div>
-    <p class="cmsmenu" style="float: left;"><div class = "menu"><?php include 'cmsmenu.php';?></div><img src="/cmsimages/parrotjig.jpg" height="80px" width="180px" border="1px"/>
+   <div class = "cmsmenu"><?php include 'cmsmenu.php';?></div>
 	</p>
-
-
-	<br />
-			<h3><strong>We Are Fishers Of Men</strong></h3>
-			<br />
-		<p align ="left">
-			
-		</p>
-		<br />
-		<p align="left">
-		</p>
-		</p>
-		<br />
-		<br />
+			<p><b>We Are Fishers Of Men</b></p>
+		<table align="center"><tr><td><img src="familyphoto.jpg" width="440" height="280" class="imgframe"></td>
+		<td>&nbsp</td>
+		<td align = "left">
+		The Fishers Of Men Project is a family run not for profit organization committed to fulfilling our calling here on earth.
+		Our mission is share the good news of Jesus Christ utilizing the metaphor of fishing. We host fishing events to teach people the
+		practical skill of fishing and we also share the word with those who attend. Additionally, we sell our hand made lures in our online
+		store which we donate <strong>100%</strong> of the proceeds to missions organizations all over the world.
+		</td>
+		<td>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</td>
+		</tr>
+		<tr>
+		<td>
+		<img src="tomanddean.jpg" width="440" height="280" class="imgframe">
+		</td>
+		<td>&nbsp</td>
+		<td align="left">
+		Our family has been fishing for over 50 years. We have fished all over the US of A. Having such a large family has enabled
+		us to compile all the best practices for catching the most fish and catching the biggest fish. Our events include many different strategy
+		tutorials as well as a great message by our pastor in the family. Our hand made lures are some of the best in the industry with years of 
+		testing design all done in house by the fisherman in the family.
+		</td>
+		<td>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</td>
+		</tr> 
+		</table>
 		<br />
 		<p class="footer">
-		<?php include 'footer.php';?>
+		<?php include 'cmsfooter.php';?>
 		</p>
 	</body>
 		</html>
