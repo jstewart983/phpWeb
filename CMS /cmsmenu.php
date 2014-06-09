563fa5f1858f28e2ffac537007b80c426bffe338
Jordan Stewart <nj.jstewart@gmail.com>
2014-06-09 00:42:08 -0400

    CMS Updates

--- CMS /cmsmenu.php
+++ CMS /cmsmenu.php
@@ -1,6 +1,7 @@
 
-<?php echo '<a href="/cmshome.php">home</a> |
-<a href="/whoweare.php">who we are</a> |
-<a href="/whatwedo.php">what we do</a> |
-<a href="/whoweserve.php">who we serve</a>';
+<?php echo '<table align = "center" ><tr><td><a href="cmshome.php"><img src="/parrotjig1.jpg" height="80px" width="180px" /></a></td><td> |
+<a href="/whoweare.php">who we are</a></td><td> |
+<a href="/whatwedo.php">what we do</a></td><td> |
+<a href="/whoweserve.php">who we serve</a></td><td> | <a href = "store.php">store</a></td></tr></table>
+<table><tr><td align="top"><a class = "button" onclick="history.go(-1);" title = "Previous Page">Back</a></td></tr></table>';
 ?>
