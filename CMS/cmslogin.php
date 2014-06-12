b1468430d65069c11d0ecbb6ce6d02aa344af833
Jordan Stewart <nj.jstewart@gmail.com>
2014-06-11 21:22:47 -0500

    fixed login session

--- CMS/cmslogin.php
+++ CMS/cmslogin.php
@@ -33,7 +33,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			session_start();
 			$_SESSION['email']=$_POST['email'];
 			$_SESSION['status']=$loggedin;
-			//$_SESSION['cart']=$cart = array();
+			$_SESSION['loggedintime']=time();
 			ob_end_clean();
 			header ('Location: store.php');
 			exit();
