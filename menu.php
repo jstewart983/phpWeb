6249a5f22561559179c4aaddfc009d2d3b3491b6
jstewart983 <sarahstewart@Sarahs-MacBook-Air.local>
2014-06-16 19:49:08 -0500

    direct family tree link to login first

--- menu.php
+++ menu.php
@@ -1,6 +1,6 @@
 
 <?php echo '<a href="/index.php">Home</a> -
-<a href="/familytree.php">Week 1</a> -
+<a href="/familytreelogin.php">Week 1</a> -
 <a href="/week2.php">Week 2</a> -
 <a href="/week3.php">Week 3</a> -
 <a href="/landingpage.php">Week 4</a> -
